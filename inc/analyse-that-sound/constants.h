f1bb6d2d642f55b2f209d464385115d720beb089
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-06-06 01:58:57 +0200

    Move constants to separate file

---
+++ inc/analyse-that-sound/constants.h
@@ -0,0 +1,18 @@
+#ifndef ANALYSE_THAT_SOUND_CONSTANTS_H
+#define ANALYSE_THAT_SOUND_CONSTANTS_H
+
+/* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Constants */
+/* -------------------------------------------------------- Boolean values -- */
+#define TRUE                    1
+#define FALSE                   0
+
+/* -------------------------------------------------------- FFT parameters -- */
+#define SAMPLE_RATE             8000
+#define FFT_POINTS_NUMBER       1024
+
+/* ---------------------------------------------- Sine function parameters -- */
+#define SINE_LOOKUP_TABLE_SIZE  128
+
+/* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ */
+
+#endif /* ANALYSE_THAT_SOUND_CONSTANTS_H */
--- src/main.c
+++ src/main.c
@@ -11,22 +11,9 @@
 #include "arm_math.h"
 #include "arm_const_structs.h"
 
-
 #include "analyse-that-sound/typedefs.h"
 #include "analyse-that-sound/macros.h"
-
-
-/* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Constants */
-/* -------------------------------------------------------- Boolean values -- */
-#define TRUE                    1
-#define FALSE                   0
-
-/* -------------------------------------------------------- FFT parameters -- */
-#define SAMPLE_RATE 	  		8000
-#define FFT_POINTS_NUMBER 		1024
-
-/* ---------------------------------------------- Sine function parameters -- */
-#define SINE_LOOKUP_TABLE_SIZE	128
+#include "analyse-that-sound/constants.h"
 
 
 /* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Global variables */
