3dc0b1aab1e6ddbe54522c9267b659afd9c974f4
Tomasz Witczak <TomaszMWitczak@gmail.com>
2019-06-10 22:46:21 +0200

    Change comments to C style

--- src/analyse-that-sound/functions/helpers.c
+++ src/analyse-that-sound/functions/helpers.c
@@ -22,28 +22,32 @@
 
 
 /* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ Helpers */
-void intToString(int value, uint8_t* pBuf, uint32_t len, uint32_t base)
+void intToString(int value,
+                 uint8_t* pBuf,
+                 uint32_t len,
+                 uint32_t base)
 {
     static const char* pAscii = "0123456789abcdefghijklmnopqrstuvwxyz";
     int pos = 0;
     int tmpValue = value;
 
-    // the buffer must not be null and at least have a length of 2 to handle one
-    // digit and null-terminator
+    /* the buffer must not be null and at least have a length of 2 to handle one
+       digit and null-terminator */
     if (pBuf == NULL || len < 2)
     {
         return;
     }
 
-    // a valid base cannot be less than 2 or larger than 36
-    // a base value of 2 means binary representation. A value of 1 would mean only zeros
-    // a base larger than 36 can only be used if a larger alphabet were used.
+    /* a valid base cannot be less than 2 or larger than 36
+       a base value of 2 means binary representation.
+       A value of 1 would mean only zeros
+       a base larger than 36 can only be used if a larger alphabet were used. */
     if (base < 2 || base > 36)
     {
         return;
     }
 
-    // negative value
+    /* negative value */
     if (value < 0)
     {
         tmpValue = -tmpValue;
@@ -51,7 +55,7 @@ void intToString(int value, uint8_t* pBuf, uint32_t len, uint32_t base)
         pBuf[pos++] = '-';
     }
 
-    // calculate the required length of the buffer
+    /* calculate the required length of the buffer */
     do {
         pos++;
         tmpValue /= base;
@@ -60,7 +64,7 @@ void intToString(int value, uint8_t* pBuf, uint32_t len, uint32_t base)
 
     if (pos > len)
     {
-        // the len parameter is invalid.
+        /* the len parameter is invalid. */
         return;
     }
 
